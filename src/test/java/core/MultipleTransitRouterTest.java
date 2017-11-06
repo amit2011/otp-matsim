fd8df47e672e697e5d55fd60bd25839def90bcd7
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-11-06 15:50:27 +0100

    ignoring otp test

--- src/test/java/core/MultipleTransitRouterTest.java
+++ src/test/java/core/MultipleTransitRouterTest.java
@@ -49,7 +49,7 @@ public class MultipleTransitRouterTest extends TransitRouterImplTest {
                 "standard",
                 "raptor",
 //                "connectionScan",
-                "otp",
+//                "otp",
 //                "r5"
         };
         return Arrays.asList(router);
